9754620de5f51c45826eaca44e8952959c4cde1f
Noah Luskey <noah@vvvvvvvvvv.io>
2024-02-22 18:07:54 -0500

    Basic initial few components of system.css wrapped and automatically use the proper css.

--- src/util.ts
+++ src/util.ts
@@ -1,15 +1,15 @@
 // https://dev.to/devsatasurion/the-best-way-to-build-big-react-components-5295
-import React from 'react';
+import React from "react";
 
 /**
  * Attaches subcomponents to a parent component for use in
  * composed components. Example:
- * 
+ *
  * <Parent>
  *    <Parent.Title>abc</Parent.Title>
  *    <Parent.Body prop1="foobar"/>
  * </Parent>
- * 
+ *
  *
  * This function also sets displayname on the parent component
  * and all children component, and has the correct return type
@@ -22,12 +22,12 @@ import React from 'react';
  */
 export function attachSubComponents<
   C extends React.ComponentType,
-  O extends Record<string, React.ComponentType>
+  O extends Record<string, React.ComponentType>,
 >(displayName: string, topLevelComponent: C, otherComponents: O): C & O {
   topLevelComponent.displayName = displayName;
   Object.values(otherComponents).forEach(
     (component) =>
-      (component.displayName = `${displayName}.${component.displayName}`)
+      (component.displayName = `${displayName}.${component.displayName}`),
   );
 
   return Object.assign(topLevelComponent, otherComponents);
