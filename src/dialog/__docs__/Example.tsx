f8c91dd3126c3ad7a4401eb1fa595dcefc8c4489
Noah Luskey <noah@vvvvvvvvvv.io>
2024-02-22 18:06:50 -0500

    implement the basic dialog

--- src/dialog/__docs__/Example.tsx
+++ src/dialog/__docs__/Example.tsx
@@ -1,7 +1,12 @@
 import React, { FC } from "react";
-import Dialog, { DialogProps } from "../index";
+import Dialog from "../index";
 
-const Example: FC<DialogProps> = (props) => {
+export type ExampleProps = {
+  line1: string;
+  line2: string;
+};
+
+const Example: FC<ExampleProps> = ({ line1, line2 }) => {
   return (
     <div
       style={{
@@ -11,12 +16,12 @@ const Example: FC<DialogProps> = (props) => {
         height: "100%",
       }}
     >
-      <Dialog>
-        <Dialog.Text>This is the first line, a header of sorts</Dialog.Text>
-        <Dialog.Text>The following lines can be very long</Dialog.Text>
-        <br/>
-        <Dialog.Text>One more line after a break</Dialog.Text>
-      </Dialog>
+      <div style={{ width: "20rem" }}>
+        <Dialog>
+          <Dialog.Text>{line1}</Dialog.Text>
+          <Dialog.Text>{line2}</Dialog.Text>
+        </Dialog>
+      </div>
     </div>
   );
 };
