46259f2acf07b0ce18809c593bfe6b03fb8f77ec
Noah Luskey <noah@vvvvvvvvvv.io>
2024-02-28 18:16:22 -0500

    2 checkboxes (#22)
    * add checkboxes
    * run formatter and update package version

---
+++ src/checkbox/__docs__/Example.tsx
@@ -0,0 +1,26 @@
+import React, { FC } from "react";
+import CheckBox from "..";
+
+export type ExampleProps = {};
+
+const Example: FC<ExampleProps> = ({}) => {
+  return (
+    <div
+      style={{
+        display: "flex",
+        justifyContent: "flex-start",
+        alignItems: "center",
+        height: "100%",
+        width: "50rem",
+      }}
+    >
+      <form>
+        <CheckBox label="A defaulted textbox" id="id1" defaultChecked />
+        <CheckBox label="This has a placeholder" id="id2" />
+        <CheckBox label="This one is disabled" id="id3" disabled />
+      </form>
+    </div>
+  );
+};
+
+export default Example;
